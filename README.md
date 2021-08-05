853c24a377a01e208dc1126ade63ffa76ab73be9
Dennis Zhang <359845097@qq.com>
2021-08-05 13:13:14 +0800

    a very simple version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rkctl
+## about 
+it's a very simple cli app, in order to use regex when executing kubectl command
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use bytes::Bytes;
 use regex::Regex;
-use std::process::Command;
+use std::{
+    io::{stdin},
+    process::{Command, Stdio},
+};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -65,8 +67,8 @@ struct DetailedResource {
 struct RawCommand {
     #[structopt(subcommand)]
     action: Action,
-    #[structopt(short, long)]
-    namespace: Option<String>,
+    #[structopt(short, long, default_value = "default")]
+    namespace: String,
 }
 
 impl RawCommand {
@@ -80,7 +82,6 @@ impl Into<ParsedCommand> for RawCommand {
         ParsedCommand {
             raw_command: self,
             regex: None,
-            result: None,
         }
     }
 }
@@ -88,39 +89,45 @@ impl Into<ParsedCommand> for RawCommand {
 struct ParsedCommand {
     raw_command: RawCommand,
     regex: Option<Regex>,
-    result: Option<Bytes>,
 }
 
 impl ParsedCommand {
     fn start(&mut self) {
         let d = self.raw_command.action.unwrap().unwrap().clone();
         self.check_regex(&d);
-        let mut cmd = Command::new("kubectl");
-        cmd.args([
-            String::from(&self.raw_command.action),
-            String::from(self.raw_command.action.unwrap()),
-        ]);
-        if let Some(namespace) = &self.raw_command.namespace {
-            cmd.args(["-n", namespace]);
-        }
-        if let Some(r) = &self.regex {
-            let mut all_res_cmd = Command::new("kubectl");
-            all_res_cmd.args([
-                "get",
-                String::from(self.raw_command.action.unwrap()).as_str(),
-            ]);
-            if let Some(namespace) = &self.raw_command.namespace {
-                all_res_cmd.args(["-n", namespace]);
+        let r = self.get_all_matched_resources();
+        match self.raw_command.action {
+            Action::Get(_) => return,
+            _ => {
+                println!("Are you sure you want to delete all these resources? [y/N]");
+                let stdin = stdin();
+                let mut yes_or_no = String::new();
+                stdin.read_line(&mut yes_or_no).unwrap();
+                if yes_or_no.starts_with("y") || yes_or_no.starts_with("Y") {
+                    for name in r {
+                        println!(
+                            "executing command kubectl {} {} {} -n {} now",
+                            String::from(&self.raw_command.action),
+                            String::from(self.raw_command.action.unwrap()),
+                            &name,
+                            &self.raw_command.namespace
+                        );
+                        let _result = Command::new("kubectl")
+                            .args([
+                                String::from(&self.raw_command.action),
+                                String::from(self.raw_command.action.unwrap()),
+                                name,
+                                "-n".to_owned(),
+                                self.raw_command.namespace.clone()
+                            ])
+                            .stdout(Stdio::inherit())
+                            .output()
+                            .expect("failed to excute command");
+                    }
+                } else {
+                    println!("exiting now...")
+                }
             }
-            let all_res = all_res_cmd.output().expect("failed to get all resources");
-            let lines: Vec<_> = all_res.stdout.split(|v| v == &b'\n').collect();
-            let mut lines = lines.into_iter();
-            println!("{:?}", lines.next().unwrap_or(b""));
-            for line in lines {
-                
-            }
-        } else {
-            cmd.arg(d.resource_name);
         }
     }
 
@@ -133,8 +140,60 @@ impl ParsedCommand {
             },
         }
     }
+
+    fn get_all_matched_resources(&mut self) -> Vec<String> {
+        let mut cmd = Command::new("kubectl");
+        cmd.args([
+            String::from(&self.raw_command.action),
+            String::from(self.raw_command.action.unwrap()),
+        ]);
+        cmd.args(["-n", &self.raw_command.namespace]);
+
+        if let Some(r) = &self.regex {
+            let mut all_res_cmd = Command::new("kubectl");
+            all_res_cmd.args([
+                "get",
+                String::from(self.raw_command.action.unwrap()).as_str(),
+            ]);
+            all_res_cmd.args(["-n", &self.raw_command.namespace]);
+            let all_res = all_res_cmd.output().expect("failed to get all resources");
+            let lines: Vec<_> = all_res.stdout.split(|v| v == &b'\n').collect();
+            let mut lines = lines.into_iter();
+            println!(
+                "{}",
+                String::from_utf8(lines.next().unwrap_or(b"").to_vec()).unwrap()
+            );
+            let mut matched_result = Vec::new();
+            for line in lines {
+                let l = line.to_owned();
+                let name = line
+                    .clone()
+                    .split(|x| x == &b' ')
+                    .collect::<Vec<_>>()
+                    .swap_remove(0);
+                let name = String::from_utf8(name.to_vec()).unwrap();
+                if r.is_match(&name) {
+                    matched_result.push(name);
+                    println!("{}", String::from_utf8(l).unwrap());
+                }
+            }
+            matched_result
+        } else {
+            cmd.arg(&self.raw_command.action.unwrap().unwrap().resource_name);
+            let res = cmd.output().expect("failed to get all resources");
+            println!("{}", String::from_utf8(res.stdout).unwrap());
+            vec![self
+                .raw_command
+                .action
+                .unwrap()
+                .unwrap()
+                .resource_name
+                .clone()]
+        }
+    }
 }
 
 fn main() {
-    let rkcmd: ParsedCommand = RawCommand::new().into();
+    let mut rkcmd: ParsedCommand = RawCommand::new().into();
+    rkcmd.start();
 }
